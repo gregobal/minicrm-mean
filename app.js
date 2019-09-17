5311666ae4c82028f8f9aef03f053948559c9676
Grigorii Balabanov <810411@mail.ru>
2019-09-17 18:49:27 +0500

    done

--- app.js
+++ app.js
@@ -32,12 +32,6 @@ app.use('/api/category', categoryRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
 
-app.get(['/', '/api/'], (req, res) => {
-  res.status('200').json({
-    "message": "Hello"
-  })
-});
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/dist/client'));
   app.get('*', (req, res) => {
